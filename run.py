2064392ded9283ce76da030b2a90663cbdab1b9d
Georgios Kopanidis <kopanidis@me.com>
2023-11-30 09:53:09 +0000

    Add function to print the game board

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 def create_board():
     """
-    Create a board with 6 rows and 7 columns
+    Create a game board with 6 rows and 7 columns
     """
     return [[' ' for _ in range(7)] for _ in range(6)]
 
+def print_board(board):
+    """
+    Prints the game board in a readable format
+    """
+    print(" 1 2 3 4 5 6 7")
+    print("---------------")
+    for row in board:
+        print("|" + "|".join(row) + "|")
+    print("---------------")
+
 board = create_board()
-print("Board after init:")
-for row in board:
-    print(row)
+print_board(board)
